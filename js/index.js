23fd4a2ea34554b37d4dd9353b4d6ecfe02f0d1d
FelixMembrive <felix.membrive@gmail.com>
2022-05-01 23:50:12 -0300

    Versão 1.0

--- js/index.js
+++ js/index.js
@@ -4,17 +4,42 @@
 // //     })
 // // }
 
-const pintarRegiao = () => {
-    document.addEventListener("click", (event, regiao) => {
-        const regiaoClicada = event.target.id
-        console.log(
-            event.target.id
-        );
-        if (event.target.id === "quadrante1" ||
-            event.target.id === "quadrante2" ||
-            event.target.id === "quadrante3" ||
-            event.target.id === "quadrante4") {
-            document.getElementById(regiaoClicada).style = "background-color: red";
-        }
-    })
-}
+const paletaVermelha = document.querySelector("#vermelho")
+paletaVermelha.style.backgroundColor = 'red'
+
+const paletaAzul = document.querySelector("#azul")
+paletaAzul.style.backgroundColor = 'blue'
+
+const paletaVerde = document.querySelector("#verde")
+paletaVerde.style.backgroundColor = 'green'
+
+const paletaAmarela = document.querySelector("#amarelo")
+paletaAmarela.style.backgroundColor = 'yellow'
+
+const paletaCiano = document.querySelector("#ciano")
+paletaCiano.style.backgroundColor = 'cyan'
+
+const paletaRoxa = document.querySelector("#roxo")
+paletaRoxa.style.backgroundColor = 'purple'
+
+
+document.getElementById("paleta-de-cores").addEventListener("click", (event) => {
+    console.log(event);
+    const regiaoClicada = event.target
+    const corClicada = regiaoClicada.getAttribute("style-color")
+    const spam = document.getElementById("cor-atual")
+    spam.setAttribute("name", corClicada)
+})
+
+
+
+document.getElementById("quadrantes").addEventListener("click", (event) => {
+    const regiaoClicada = event.target.id
+    if (regiaoClicada.includes("quadrante") == true) {
+        console.log(regiaoClicada);
+        const spam = document.getElementById("cor-atual")
+        const corAtual = spam.getAttribute("name")
+        console.log(corAtual);
+        document.getElementById(regiaoClicada).style.backgroundColor = corAtual
+    }
+})
